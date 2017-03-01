39690ec1b891349a00d131fa3b17edad604d7d27
Jmathieu3289 <Justin.Mathieu@sendiksmarket.com>
2017-03-01 16:56:04 -0600

    Working on mobile

--- www/js/controller.js
+++ www/js/controller.js
@@ -26,36 +26,30 @@ mainApp.controller('MainCtrl', function ($scope, $http) {
 
     $scope.stores = ["Bayside", "Corners", "Elm Grove", "Franklin", "Germantown", "Grafton", "Greendale", "Greenfield", "Hales Corner",
     "Hartland", "Lilly Road", "Mequon", "New Berlin", "Wauwatosa", "West Bend", "West Milwaukee", "Whitefish Bay"];
+    $scope.shorts = ["bsd","crnrs", "elm", "frk", "gtn", "grf", "gdale", "grn", "hales", "hart", "lilly", "meq", "nbn", "tosa", "wbd", "wmke", "wfb"];
     $scope.selectedStore = null;
 
     $scope.pageState = 0;
     $scope.requestType = null;
+    $scope.requestName = null;
 
     /* Methods */
     /* These are functions that are not visible to the page. Used for doing things like loading data, etc */
-
-    function getDepartments() {
-        $http({
-            url: "/api/department",
-            method: "GET"
-        }).then(function (response) {
-            $scope.departments = response.data['results'];
-        });
-    }
 
     /* Commands */
     /* These are functions that can be tied to the page, such as on button clicks */
 
     $scope.addManualTag = function() {
-        if($scope.tags.indexOf($scope.manualTag) == -1){
+        if($scope.tags.indexOf($scope.manualTag) == -1 && $scope.manualTag != null){
             $scope.tags.push($scope.manualTag);
         }
         $scope.manualTag = null;
     };
 
     $scope.scan = function(data) {
-        if($scope.tags.indexOf(data) == -1){
+        if($scope.tags.indexOf(data) == -1 && $scope.pageState == 0){
             $scope.tags.push(data);
+            $scope.$digest();
         }
     };
 
@@ -66,13 +60,36 @@ mainApp.controller('MainCtrl', function ($scope, $http) {
     $scope.toStoreSelection = function() {
         if($scope.tags.length > 0){
             $scope.pageState = 1;
+            document.getElementById('requestName').value = '';
         }
     };
 
     $scope.submit = function() {
-        console.log($scope.tags);
-        console.log($scope.selectedStore);
-        console.log($scope.requestType);
+
+        $scope.pageState = 2;
+
+        if($scope.selectedStore == null || $scope.requestType == null || $scope.requestName == null) {
+            alert("Missing information: check name, store, and type and try again.");
+            return;
+        }
+
+        $http({
+            url: "http://dev.sendiks.co/ios/upload-tag.php",
+            method: "POST",
+            data: {
+                "filename": $scope.requestName + "_" + $scope.shorts[$scope.stores.indexOf($scope.selectedStore)] + "_" + $scope.requestType,
+                "tag-list": $scope.tags.join('\n')
+            }
+        }).then(function (response) {
+            if(response.data == 0){
+                alert("Tag request submitted!");
+                location.reload();
+            }else{
+                $scope.pageState = 1;
+                alert("An error occured. Make sure you have a network connection and try again");
+            }
+        });
+
     };
 
     /* Constructor */
